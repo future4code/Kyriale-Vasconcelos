06b9a7ea88ddbac37cdbe96ecc0fa61a013cb031
Kyriale Morant-Cavalcanti <kyrialevm@gmail.com>
2021-10-07 23:51:23 -0300

    exercícios funções dia 07/10/2021

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,39 @@
+//Exercícios de interpretação de código
+
+//1. Leia o código abaixo
+ /*    
+    ```jsx
+    function minhaFuncao(variavel) {
+    	return variavel * 5
+    }
+    
+    console.log(minhaFuncao(2))
+    console.log(minhaFuncao(10))
+    ```
+    
+    a) O que vai ser impresso no console?
+    
+    b) O que aconteceria se retirasse os dois `console.log` e simplesmente invocasse a função `minhaFuncao(2)` e `minhaFuncao(10)`? O que apareceria no console? */
+
+    // a) 10 e 50
+    // b) nada
+
+//2. Leia o código abaixo
+    /* 
+    ```jsx
+    let textoDoUsuario = prompt("Insira um texto");
+    
+    const outraFuncao = function(texto) {
+    	return texto.toLowerCase().includes("cenoura")
+    }
+    
+    const resposta = outraFuncao(textoDoUsuario)
+    console.log(resposta)
+    ```
+    
+    a. Explique o que essa função faz e qual é sua utilidade
+    
+    b. Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+         i.   `Eu gosto de cenoura`
+         ii.  `CENOURA é bom pra vista`
+         iii. `Cenouras crescem na terra` */

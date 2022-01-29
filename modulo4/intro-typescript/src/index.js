705cd53404271d8ee924db2ef9a3652665cf1662
Kyriale Morant-Cavalcanti <kyrialevm@gmail.com>
2022-01-28 21:09:08 -0300

    exercício 01, n sei pq criou um arquivo novo, mas funcinou, rodei os comandos tsc index.ts . e node index.js

--- modulo4/intro-typescript/src/index.js
+++ modulo4/intro-typescript/src/index.js
@@ -9,15 +9,15 @@
 //       return "Isósceles";
 //     }
 //   }
-
-function checaTriangulo(a: number, b: number, c: number):string {
+function checaTriangulo(a, b, c) {
     if (a !== b && b !== c) {
-      return "Escaleno";
-    } else if (a === b && b === c) {
-      return "Equilátero";
-    } else {
-      return "Isósceles";
+        return "Escaleno";
+    }
+    else if (a === b && b === c) {
+        return "Equilátero";
+    }
+    else {
+        return "Isósceles";
     }
 }
-
-console.log(checaTriangulo(1, 2, 2))
+console.log(checaTriangulo(1, 2, 2));

2293357b8cd68155b1c5a2821495f4cc1e34f632
Kyriale Morant-Cavalcanti <kyrialevm@gmail.com>
2022-01-30 19:32:56 -0300

    exercício 2, está dando erro de digitação com ' : ' , mas n entendi onde estaria sobrando os 2 pontos

---
+++ modulo4/intro-typescript/src/exercicio-2.ts
@@ -0,0 +1,18 @@
+// 2) A seguinte função em JavaScript pergunta ao usuário suas três cores favoritas e imprime no console um array que contenha essas três cores. Refatore a função para o Typescript.
+
+// function imprimeTresCoresFavoritas() {
+//     const cor1 = prompt("Insira sua primeira cor favorita")
+//     const cor2 = prompt("Insira sua segunda cor favorita")
+//     const cor3 = prompt("Insira sua terceira cor favorita")
+//     console.log([cor1, cor2, cor3])
+//  }
+
+function imprimeTresCoresFavoritas():void {
+    let cor1:string = process.argv[2]
+    let cor2:string = process.argv[3]
+    let cor3:string = process.argv[4]
+
+    console.log([cor1, cor2, cor3]) 
+}
+
+imprimeTresCoresFavoritas()
